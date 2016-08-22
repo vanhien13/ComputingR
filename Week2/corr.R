b1d6f593c6007fb2bb5f93404cfff9cc004fbe14
vanhien13 <vanhien13@gmail.com>
2016-08-22 16:07:04 +0700

    Update corr.R

--- Week2/corr.R
+++ Week2/corr.R
@@ -8,45 +8,14 @@ corr <- function(directory, threshold = 0) {
   ## nitrate and sulfate; the default is 0
   
   ## Return a numeric vector of correlations
-  
-  files <- list.files( path = directory )
-  #filenames <- list.files("temp", pattern="*.csv", full.names=TRUE)
-
-  correlation <- c()
-  
-  for(i in 1:length(files)){
-    data <- read.csv( paste(directory, files[i], sep=""))
-    data <- data[complete.cases(data),]
-    if ( nrow(data) > threshold ) {
-      correlation <- c(correlation, cor(data$sulfate, data$nitrate) ) # append correlations
+  file_list <- list.files(path = directory)
+  correl <- c()
+  for(i in 1:length(file_list)){
+    data <- read.csv(paste(directory, file_list[i], sep=""))
+    data[complete.cases(data),]
+    if(nrow(data)>threshold){
+      correl <- c(correl, cor(data$sulfate, data$nitrate)) # append correlations
     }
   }
-  
-  return( correlation )
+  return(correl)  
 }
-
-output <- corr("specdata/", threshold = 3)
-
-#filenames <- list.files("C:/R_Week2/specdata/specdata/", pattern="*.csv", full.names=TRUE)
-#ldf <- lapply(filenames, read.csv)
-#res <- lapply(ldf, summary)
-#names(res) <- substr(filenames, 6, 30)
-#setwd("target_dir/")
- 
-#file_list <- list.files()
- 
-#for (file in file_list){
-       
-  # if the merged dataset doesn't exist, create it
-#  if (!exists("dataset")){
-#    dataset <- read.table(file, header=TRUE, sep="\t")
-#  }
-   
-  # if the merged dataset does exist, append to it
-#  if (exists("dataset")){
-#    temp_dataset <-read.table(file, header=TRUE, sep="\t")
-#    dataset<-rbind(dataset, temp_dataset)
-#    rm(temp_dataset)
-#  }
- 
-#}
